e810374938c8fb8c8f83f9083944f8f59bcdb419
frognar <sebastian.przyszlak@gmail.com>
2025-02-01 11:04:40 +0100

    remember savable

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -8,8 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,15 +16,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableIntStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text(
                 text = "You've had $count glasses."
             )
@@ -36,12 +28,6 @@ fun WaterCounter(modifier: Modifier = Modifier) {
                 enabled = count < 10
             ) {
                 Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text("Clear water count")
             }
         }
     }
